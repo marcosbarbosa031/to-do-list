24f36afd8809610adcd759e28843f9d341bd3d28
Marcos Barbosa <marcosbarbosa031@gmail.com>
2020-06-15 19:54:33 -0300

    Adding logic to type and edit list title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToDoListComponent } from './components/to-do-list/to-do-list.component';
@@ -16,7 +16,8 @@ import { ToDoComponent } from './page';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/to-do-list/to-do-list.component.ts
+++ src/app/components/to-do-list/to-do-list.component.ts
@@ -15,15 +15,48 @@ export class ToDoListComponent implements OnInit {
   ngOnInit() {
   }
 
-  addList() {
+  async addList(element: HTMLElement) {
     const list: ToDoListType = {
       id: this.toDoList.length + 1,
       title: '',
-      description: '',
+      description: 'Nova lista...',
       todos: []
     };
 
     this.toDoList.push(list);
+    await this.sleep(100);
+    const titleInput = element.parentElement.parentElement.children[1].children[0].children[0] as HTMLElement;
+    titleInput.focus();
+  }
+
+  sleep(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  disableInput(element: HTMLElement) {
+    console.log(element);
+    const hiddenTitle = element.parentElement.children[1];
+    element.setAttribute('disabled', 'true');
+    element.setAttribute('hidden', 'true');
+    hiddenTitle.removeAttribute('hidden');
+  }
+
+  enableInput(element: HTMLElement) {
+    const inputElement = element.parentElement.children[0] as HTMLElement;
+    element.setAttribute('hidden', 'true');
+    inputElement.removeAttribute('disabled');
+    inputElement.removeAttribute('hidden');
+    inputElement.focus();
+  }
+
+  endEdit(event) {
+    if (event.key === 'Enter') {
+      const element = event.target;
+      const hiddenTitle = element.parentElement.children[1];
+      element.setAttribute('disabled', 'true');
+      element.setAttribute('hidden', 'true');
+      hiddenTitle.removeAttribute('hidden');
+    }
   }
 
   removeList(id: number) {
